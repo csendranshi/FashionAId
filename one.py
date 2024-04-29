71519effdb3a305a3e8e148867fecf1bc7ec5b86
Sitanshu Kushwaha <sitanshu.kushwaha@live.com>
2024-04-28 21:07:02 -0400

    added weather api

--- one.py
+++ one.py
@@ -1,12 +1,28 @@
 import streamlit as st
 import google.generativeai as genai
 import pymongo
-from config import api_key
+from config import streamlit_api_key, weather_api_key
 import requests
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["FashionAId-DB"]
 my_profiles = db["Profiles"]
+
+lat = "40.7209"
+lon = "74.0007"
+
+url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_key}&cnt=1"
+
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Print the response content (usually in JSON format)
+    data = response.json()
+else:
+    # Print an error message if the request was not successful
+    print("Error:", response.status_code)
+
 
 
 st.title('Outfit Recommendation App 👗')
@@ -49,6 +65,7 @@ particular occasion:{occasion},
 aesthetic:{aesthetic} ,
 information enclosed in wardrobe: {my_profile_details},
 extra information:{extra} ,
+Weather info: {data['list']}
 If either of the instruction is not present use the best of your judgement to assume it, 
 use as many fashion blogs as you want to make a decision and 
 give the top 10 results bulletted in seperate lines
@@ -56,7 +73,7 @@ give the top 10 results bulletted in seperate lines
 
 '''
 
-genai.configure(api_key=api_key)
+genai.configure(api_key=streamlit_api_key)
 model = genai.GenerativeModel(model_name = "gemini-pro")
 
 
